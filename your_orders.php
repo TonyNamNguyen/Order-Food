3c7016dbfd74c12df907a32605b8e5c9509b92d4
Karry_Pham <85409706+hieuliempham@users.noreply.github.com>
2024-04-18 23:33:06 +0700

    update UX

--- your_orders.php
+++ your_orders.php
@@ -364,6 +364,14 @@ td, th {
     <script src="js/jquery.isotope.min.js"></script>
     <script src="js/headroom.js"></script>
     <script src="js/foodpicky.min.js"></script>
+	<script src="js/jquery.min.js"></script>
+    <script src="js/tether.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/animsition.min.js"></script>
+    <script src="js/bootstrap-slider.min.js"></script>
+    <script src="js/jquery.isotope.min.js"></script>
+    <script src="js/headroom.js"></script>
+    <script src="js/foodpicky.min.js"></script>
 </body>
 
 </html>
